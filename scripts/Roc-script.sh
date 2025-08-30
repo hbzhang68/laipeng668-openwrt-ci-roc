4dd5d9d30dab248e8ccbb522638af1ae08ccba20
hbzhang68 <72781967+hbzhang68@users.noreply.github.com>
2025-08-30 09:07:07 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -49,6 +49,8 @@ chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app
 git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki package/OpenWrt-nikki
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
 git clone --depth=1 https://github.com/sirpdboy/luci-app-partexp package/luci-app-partexp
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo package/OpenWrt-momo
+git clone --depth=1 https://github.com/sirpdboy/luci-app-netspeedtest package/luci-app-netspeedtest
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
